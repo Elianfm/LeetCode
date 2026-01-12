183db61bebc4bd5555743fe2629bd959c6950394
Elianfm <elianfmujica@gmail.com>
2026-01-12 16:50:18 -0500

    Runtime 10ms beats 99.19%

--- src/217. Contains Duplicates/ContainsDuplicate.java
+++ src/217. Contains Duplicates/ContainsDuplicate.java
@@ -4,21 +4,39 @@ import java.util.Set;
 public class ContainsDuplicate {
 
     /*
+     * Second try, in docs, hashset returns true or false when adding an element
+     * according to whether the element already exists in the set, so we can use
+     * that to simplify the code.
+     * 
+     * Runtime 10ms beats 99.19%
+     */
+    public boolean containsDuplicate2(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int n : nums) {
+            if (!set.add(n))
+                return true;
+        }
+
+        return false;
+    }
+
+    /*
      * First try, using HashSet to store the elements in the array.
      * 
      * Runtime 11ms beats 70.92%
      */
 
-     class Solution {
-        public boolean containsDuplicate(int[] nums) {
-            Set<Integer> exist = new HashSet<>();
-    
-            for(int i = 0; i<nums.length; i++){
-                int actual = nums[i];
-                if(exist.contains(actual)) return true;
-                exist.add(actual);
-            }
-            return false;
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> exist = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int actual = nums[i];
+            if (exist.contains(actual))
+                return true;
+            exist.add(actual);
         }
+        return false;
     }
+
 }
