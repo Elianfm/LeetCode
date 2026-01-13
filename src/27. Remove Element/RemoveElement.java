c74a16c0bf56e266a52e79eb4a51e3458a364cba
Elianfm <elianfmujica@gmail.com>
2026-01-13 11:47:20 -0500

    Runtime 0ms beats 100%

--- src/27. Remove Element/RemoveElement.java
+++ src/27. Remove Element/RemoveElement.java
@@ -1,3 +1,27 @@
+/*
+* Alternative solution
+*
+* Runtime 0ms beats 100%
+*/
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int last = nums.length;
+
+        int idx = 0;
+        while (idx < last) {
+            int actual = nums[idx++];
+            if (actual != val) continue;
+            
+            nums[--idx] = nums[--last];
+            nums[last] = actual;
+        }
+
+        return last;
+    }
+}
+
+
+
 class RemoveElement {
     public int removeElement(int[] nums, int val) {
         int count = 0;
