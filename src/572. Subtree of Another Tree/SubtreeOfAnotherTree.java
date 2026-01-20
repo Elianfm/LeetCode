42cdf82a2850a1fce8df9561ffbcf405a04184c2
Elianfm <elianfmujica@gmail.com>
2026-01-20 10:53:04 -0500

    Runtime 2ms beats 99.06%

--- src/572. Subtree of Another Tree/SubtreeOfAnotherTree.java
+++ src/572. Subtree of Another Tree/SubtreeOfAnotherTree.java
@@ -28,4 +28,17 @@ public class SubtreeOfAnotherTree {
         }
 
     }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
