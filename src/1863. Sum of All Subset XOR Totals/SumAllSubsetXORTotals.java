848625b4172517b6abaafc303adf73ce0886ce09
Elianfm <elianfmujica@gmail.com>
2026-01-20 10:59:27 -0500

    Runtime: 0ms beats 100.00%

--- src/1863. Sum of All Subset XOR Totals/SumAllSubsetXORTotals.java
+++ src/1863. Sum of All Subset XOR Totals/SumAllSubsetXORTotals.java
@@ -1,3 +1,21 @@
 public class SumAllSubsetXORTotals {
 
+    /*
+     * First try, recursive solution to explore all subsets and calculate the 
+     * xor for each one possible.
+     * Runtime: 0ms beats 100.00%
+     */
+    class Solution {
+        public int subsetXORSum(int[] nums) {
+            return subPos(nums, 0, 0);
+        }
+
+        public int subPos(int[] nums, int pos, int xorActual) {
+            if (pos >= nums.length) return xorActual;
+
+            return subPos(nums, pos + 1, xorActual) +
+                    (subPos(nums, pos + 1, xorActual ^ nums[pos]));
+        }
+
+    }
 }
