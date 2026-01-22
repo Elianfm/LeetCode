71fd35fed6d233e2d432de93d92f664580320f54
Elianfm <elianfmujica@gmail.com>
2026-01-22 12:28:29 -0500

    runtime 0ms beats 100%

---
+++ src/268. Missing Number/MissingNumber.java
@@ -0,0 +1,36 @@
+public class MissingNumber{
+
+    /*
+     * Alr, i got the bit manipulation approach
+     * runtime 0ms beats 100%
+     */
+    class Solution {
+        public int missingNumber(int[] nums) {
+            int miss = nums.length;
+            for(int i = 0; i<nums.length; i++)
+                miss ^= i^nums[i];
+
+            return miss;
+        }
+    }
+
+    /*
+     * First try, after some tries to solve this with bit manipulation i 
+     * got the math approach. (???)
+     * 
+     * Runtime 0ms beats 100%
+     */
+    class Solution {
+        public int missingNumber(int[] nums) {
+            int miss = 0;
+            int len = nums.length;
+            int sum = (len * (len + 1))/2;
+
+            for(int i = 0; i<len; i++)
+                miss += nums[i];
+
+            return sum-miss;
+        }
+    }
+
+}
