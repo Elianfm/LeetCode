f8c01093473f8a6d8f95163078ae4e53fcc7c8cf
Elianfm <elianfmujica@gmail.com>
2026-01-26 14:21:43 -0500

    Runtime: 0ms beats 100%

--- src/206. Reverse Linked List/ReverseLinkedList.java
+++ src/206. Reverse Linked List/ReverseLinkedList.java
@@ -1,37 +1,39 @@
-class Solution {
-    /*
-    * First try, loop and create new nodes for the reversed list
-    * Runtime: 0ms beats 100%
-    */
-    public ListNode reverseList(ListNode head) {
-        if (head == null)
-            return null;
-        ListNode prev = null;
-        ListNode actual = head;
+public class ReverseLinkedList {
+    class Solution {
+        /*
+         * First try, loop and create new nodes for the reversed list
+         * Runtime: 0ms beats 100%
+         */
+        public ListNode reverseList(ListNode head) {
+            if (head == null)
+                return null;
+            ListNode prev = null;
+            ListNode actual = head;
 
-        while (actual != null) {
-            ListNode newNode = new ListNode(actual.val, prev);
-            prev = newNode;
-            actual = actual.next;
+            while (actual != null) {
+                ListNode newNode = new ListNode(actual.val, prev);
+                prev = newNode;
+                actual = actual.next;
+            }
+
+            return prev;
         }
 
-        return prev;
-    }
+        public class ListNode {
+            int val;
+            ListNode next;
 
-    public class ListNode {
-        int val;
-        ListNode next;
+            ListNode() {
+            }
 
-        ListNode() {
-        }
+            ListNode(int val) {
+                this.val = val;
+            }
 
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
+            ListNode(int val, ListNode next) {
+                this.val = val;
+                this.next = next;
+            }
         }
     }
 }
