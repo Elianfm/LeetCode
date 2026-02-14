c5c27d6368333b7bc1eec6a260da9cef76cee035
Elianfm <elianfmujica@gmail.com>
2026-02-14 12:02:56 -0500

    Runtime: 2ms beats 95.13%

--- src/39. Combination Sum/CombinationSum.java
+++ src/39. Combination Sum/CombinationSum.java
@@ -3,6 +3,42 @@ import java.util.List;
 
 public class CombinationSum {
 
+
+    /*
+     * Second try, similar approach, i add the ls directly in the while and then i remove it after the while,
+     * this way i avoid to do a lot of backtracking when the sum exceed the target.
+     * 
+     * Runtime: 2ms beats 95.13%
+     */
+    class Solution2 {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ls = new ArrayList<>();
+
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            explore(candidates, target, 0, 0);
+            return res;
+        }
+
+        private void explore(int[] candidates, int target, int sum, int pos){
+            if(pos >= candidates.length) return;
+
+            int actual = candidates[pos];
+            int count = 0;
+
+            while(sum < target){
+                explore(candidates, target, sum, pos+1);
+                ls.add(actual);
+                sum += actual;
+                count++;
+
+                if(sum == target) res.add(new ArrayList<Integer>(ls));
+            }
+
+            while(count-- > 0)
+                ls.remove(ls.size()-1);
+        }
+    }
+
     /*
      * First try, i use backtracking, two paths: one where I take the number
      * and another where I don't take it. I use a while to keep taking the same number
