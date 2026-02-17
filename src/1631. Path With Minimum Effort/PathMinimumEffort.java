aa83bc53f7ec44ac77168028e7866c6e07cb750f
Elianfm <elianfmujica@gmail.com>
2026-02-17 09:43:32 -0500

    Runtime 73ms beats 16.72%

--- src/1631. Path With Minimum Effort/PathMinimumEffort.java
+++ src/1631. Path With Minimum Effort/PathMinimumEffort.java
@@ -1,6 +1,78 @@
+import java.util.PriorityQueue;
+
 public class PathMinimumEffort {
 
-    // pending to try with Dijkstra
+    /*
+     * Second try, Dijkstra algorithm but slow
+     * 
+     * Runtime 73ms beats 16.72%
+     * 
+     */
+    class Solution2 {
+        
+        boolean[][] visited;
+        PriorityQueue<MyNode> pq;
+
+        public int minimumEffortPath(int[][] heights) {
+            pq = new PriorityQueue<>( (a,b) ->
+                Integer.compare(a.cost, b.cost) );
+            visited = new boolean[heights.length][heights[0].length];
+
+            pq.offer(new MyNode(0,0,0));
+
+            while(true){
+                MyNode actual = pq.poll();
+                if(visited[actual.row][actual.col]) continue;
+
+                if(actual.row == heights.length-1 && actual.col == heights[0].length-1)
+                    return actual.cost;
+
+                visited[actual.row][actual.col] = true;
+                addCosts(heights, actual.row, actual.col, actual);
+            }
+
+
+
+        }
+
+        private void addCosts(int[][] heights, int row, int col, MyNode origin){
+            int actual = heights[row][col];
+
+            if(row < heights.length - 1){
+                int bestCost = Math.max(Math.abs(actual - heights[row+1][col]), origin.cost);
+                pq.offer(new MyNode(bestCost, row+1, col));
+            }
+
+            if(col < heights[0].length - 1){
+                int bestCost = Math.max(Math.abs(actual - heights[row][col+1]), origin.cost);
+                pq.offer(new MyNode(bestCost, row, col+1));
+            }
+
+            if(row > 0){
+                int bestCost = Math.max(Math.abs(actual - heights[row-1][col]), origin.cost);
+                pq.offer(new MyNode(bestCost, row-1, col));
+            }
+
+
+            if(col > 0){
+                int bestCost = Math.max(Math.abs(actual - heights[row][col-1]), origin.cost);
+                pq.offer(new MyNode(bestCost, row, col-1));
+            }
+        }
+
+
+        private class MyNode{
+            int cost;
+            int row;
+            int col;
+
+            public MyNode(int cost, int row, int col){
+                this.cost = cost;
+                this.row = row;
+                this.col = col;
+            }
+        }
+    }
 
     /*
      * First try, TLE raw unclean ugly solution
