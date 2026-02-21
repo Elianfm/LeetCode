24b6fcf5094f658fdc3d7bcce88385ea269c17d7
Elianfm <elianfmujica@gmail.com>
2026-02-21 11:10:10 -0500

    Runtime 15ms beats 62.11%

--- src/5. Longest Palindromic Substring/LongestPalindromicSubstring.java
+++ src/5. Longest Palindromic Substring/LongestPalindromicSubstring.java
@@ -1,6 +1,55 @@
 public class LongestPalindromicSubstring {
 
     /*
+     * Second try
+     * Runtime 15ms beats 62.11%
+     */
+    class Solution {
+        int len;
+
+        public String longestPalindrome(String s) {
+            
+            int maxLong = 0;
+            String longestSubstring = "";
+
+            len = s.length();
+
+            for(int i = 0; i<len; i++){
+                int uneven = repTimes(s, i-1, i+1);
+                int even = repTimes(s, i-1, i);
+
+                int totalUneven = (uneven*2)+1;
+                int totalEven = (even*2);
+
+                if(totalUneven > maxLong){
+                    maxLong = totalUneven;
+                    longestSubstring = s.substring(i-uneven, i+uneven+1);
+                }   
+
+                if(totalEven > maxLong){
+                    maxLong = totalEven;
+                    longestSubstring = s.substring(i-even, i+even);
+                } 
+            }
+
+            return longestSubstring;
+
+        }
+
+        private int repTimes(String s, int left, int right){
+            if(left < 0 || right == len) return 0;
+            int actualCount = 0;    
+
+            while (left >= 0 && right < len && s.charAt(left--) == s.charAt(right++))
+                actualCount++;
+            
+            return actualCount;
+
+        }
+    }
+
+
+    /*
      * 
      * Firs try, i looped through the string and select two pointers to move from
      * the center to the sides
