726f5b735ebc7c59e248e4d63e884d727dd5994c
Elianfm <elianfmujica@gmail.com>
2026-02-21 10:15:48 -0500

    Runtime 1ms beats 100%

--- src/695. Max Area of Island/MaxAreaIsland.java
+++ src/695. Max Area of Island/MaxAreaIsland.java
@@ -8,6 +8,37 @@ public class MaxAreaIsland {
      * Runtime 1ms beats 100%
      */
 
+    class Solution2 {
+        int lenRow;
+        int lenCol;
+
+        public int maxAreaOfIsland(int[][] grid) {
+            int maxArea = 0;
+            lenRow = grid.length;
+            lenCol = grid[0].length;
+
+            for(int i = 0; i<grid.length; i++)
+                for(int j = 0; j<grid[0].length; j++)
+                    if(grid[i][j] == 1)
+                        maxArea = Math.max(maxArea, findArea(grid, i, j));
+
+            return maxArea;
+            
+        }
+
+        private int findArea(int[][] grid, int row, int col){
+            if(col < 0 || row < 0 || row >= lenRow || col >= lenCol 
+                || grid[row][col] == 0) return 0;
+
+            grid[row][col] = 0;
+
+            return 1 + findArea(grid, row+1, col)
+                            + findArea(grid, row-1, col)
+                            + findArea(grid, row, col+1)
+                            + findArea(grid, row, col-1);
+        }
+    }
+
     /*
      * First try, pretty similar to 200. Number of Islands, 
      * but instead of counting the number of islands, we count the area 
