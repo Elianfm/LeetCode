36ffb98058250d46584acf286c350eb34ff978c9
Elianfm <elianfmujica@gmail.com>
2026-02-22 11:16:44 -0500

    Runtime 36ms beats 99.15%

--- src/1631. Path With Minimum Effort/PathMinimumEffort.java
+++ src/1631. Path With Minimum Effort/PathMinimumEffort.java
@@ -1,6 +1,149 @@
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class PathMinimumEffort {
+
+    /*
+     * Cleaner but slightly slower, similar to solution3 
+     * Runtime 43ms beats 53.69%
+     */
+    class Solution4 {
+        PriorityQueue<MyNode> next = new PriorityQueue<>((a,b) -> Integer.compare(a.cost, b.cost));
+        int[][] heights;
+        int[][] costs;
+
+        public int minimumEffortPath(int[][] heights) {
+            this.heights = heights;
+            this.costs = new int[heights.length][heights[0].length];
+            next.offer(new MyNode(0,0,0));
+            costs[0][0] = 0;
+            
+            for(int i = 0; i<heights.length; i++) 
+                Arrays.fill(costs[i], Integer.MAX_VALUE);
+
+            while(true){
+                MyNode n = next.poll();
+                if(n.cost > costs[n.row][n.col]) continue;
+                if(n.row == heights.length-1 && n.col == heights[0].length-1) return n.cost;
+                if(n.col < heights[0].length-1) explore(n, n.row, n.col+1);
+                if(n.col > 0) explore(n, n.row, n.col-1);
+                if(n.row < heights.length-1) explore(n, n.row+1, n.col);
+                if(n.row > 0) explore(n, n.row-1, n.col);
+            }
+
+        }
+
+        private void explore(MyNode n, int row, int col){
+            int cellCost = Math.max(n.cost,
+                    Math.abs(heights[row][col] - heights[n.row][n.col]));
+
+            if(cellCost < costs[row][col]){
+                next.offer(new MyNode(row, col, cellCost));
+                costs[row][col] = cellCost;
+            }
+        }
+
+        private class MyNode{
+            int row;
+            int col;
+            int cost;
+
+            public MyNode(int row, int col, int cost){
+                this.row = row;
+                this.col = col;
+                this.cost = cost;
+            }
+        }
+    }
+
+
+    /*
+     * Third try, Dijkstra algorithm, faster
+     * Runtime 36ms beats 99.15%
+     */
+    class Solution3 {
+        
+        PriorityQueue<MyNode> pq;
+        int[][] dist;
+
+        public int minimumEffortPath(int[][] heights) {
+            pq = new PriorityQueue<>( (a,b) ->
+                Integer.compare(a.cost, b.cost) );
+        
+
+            dist = new int[heights.length][heights[0].length];
+
+            for(int i = 0; i < heights.length; i++){
+                Arrays.fill(dist[i], Integer.MAX_VALUE);
+            }
+
+            dist[0][0] = 0;
+            pq.offer(new MyNode(0,0,0));
+
+            while(true){
+                MyNode actual = pq.poll();
+                if(actual.cost > dist[actual.row][actual.col]) continue;
+
+                if(actual.row == heights.length-1 && actual.col == heights[0].length-1)
+                    return actual.cost;
+
+                addCosts(heights, actual.row, actual.col, actual);
+            }
+
+
+
+        }
+
+        private void addCosts(int[][] heights, int row, int col, MyNode origin){
+            int actual = heights[row][col];
+
+            if(row < heights.length - 1){
+                int bestCost = Math.max(Math.abs(actual - heights[row+1][col]), origin.cost);
+                if(bestCost < dist[row+1][col]){
+                    dist[row+1][col] = bestCost;
+                    pq.offer(new MyNode(bestCost, row+1, col));
+                }
+            }
+
+            if(col < heights[0].length - 1){
+                int bestCost = Math.max(Math.abs(actual - heights[row][col+1]), origin.cost);
+                if(bestCost < dist[row][col+1]){
+                    dist[row][col+1] = bestCost;
+                    pq.offer(new MyNode(bestCost, row, col+1));
+                }
+            }
+
+            if(row > 0){
+                int bestCost = Math.max(Math.abs(actual - heights[row-1][col]), origin.cost);
+                if(bestCost < dist[row-1][col]){
+                    dist[row-1][col] = bestCost;
+                    pq.offer(new MyNode(bestCost, row-1, col));
+                }
+            }
+
+            if(col > 0){
+                int bestCost = Math.max(Math.abs(actual - heights[row][col-1]), origin.cost);
+                if(bestCost < dist[row][col-1]){
+                    dist[row][col-1] = bestCost;
+                    pq.offer(new MyNode(bestCost, row, col-1));
+                }
+            }
+        }
+
+
+        private class MyNode{
+            int cost;
+            int row;
+            int col;
+
+            public MyNode(int cost, int row, int col){
+                this.cost = cost;
+                this.row = row;
+                this.col = col;
+            }
+        }
+    }
+
 
     /*
      * Second try, Dijkstra algorithm but slow
