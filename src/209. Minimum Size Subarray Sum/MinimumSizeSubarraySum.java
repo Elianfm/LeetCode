715eeedb1e5237d564035a335d7247db50ff52f4
Elianfm <elianfmujica@gmail.com>
2026-03-04 13:06:12 -0500

    Runtime 1ms beats 99.77%

--- src/209. Minimum Size Subarray Sum/MinimumSizeSubarraySum.java
+++ src/209. Minimum Size Subarray Sum/MinimumSizeSubarraySum.java
@@ -1,7 +1,66 @@
-/*
- * PENDING TO SOLVE IT IN O(n) TIME
- */
+
 class MinimumSizeSubarraySum {
+
+    /*
+     * Cleaner version of second try, i dont need two loops to find the minimum size of the subarray that sums up to target,
+     * 
+     * Runtime 1ms beats 99.77%
+     */
+    class Solution2 {
+        public int minSubArrayLen(int target, int[] nums) {
+            int minCount = Integer.MAX_VALUE;
+            int actualSum = 0;
+            int rightIdx = 0;
+            int leftIdx = 0;
+            
+            while(rightIdx < nums.length){
+                actualSum += nums[rightIdx++];
+                while(actualSum >= target){
+                    minCount = Math.min(minCount, rightIdx-leftIdx);
+                    actualSum -= nums[leftIdx++];            
+                }
+            }
+
+            return (minCount == Integer.MAX_VALUE) ? 0 : minCount;
+
+        }
+    }
+
+    /*
+     * Second try, using sliding window, two pointers and a while loop to find the minimum 
+     * size of the subarray that sums up to target.
+     * 
+     * Runtime: 1ms beats 99.77%
+     */
+    class Solution {
+        public int minSubArrayLen(int target, int[] nums) {
+            int minCount = 0;
+            int actualSum = 0;
+            int rightIdx = 0;
+            int leftIdx = 0;
+            
+            while(rightIdx < nums.length && actualSum < target){
+                actualSum += nums[rightIdx];
+                rightIdx++;
+            }
+
+            if(actualSum < target) return 0;
+
+            minCount = rightIdx;
+            while(true){
+                while(actualSum >= target){
+                    actualSum -= nums[leftIdx++];            
+                    minCount = Math.min(minCount, rightIdx-leftIdx+1);
+                }
+                if(rightIdx >= nums.length) break;
+                actualSum += nums[rightIdx++];
+            }
+
+            return minCount;
+
+        }
+    }
+
 
     /*
      * First try, I solve it but it's extremely slow 
