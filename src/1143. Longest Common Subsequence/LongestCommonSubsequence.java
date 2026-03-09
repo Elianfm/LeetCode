438ee4c47e7a63f4f3969d96b596b244121e4aac
Elianfm <elianfmujica@gmail.com>
2026-03-09 11:02:12 -0500

    Runtime: 25ms beats 29.93%

--- src/1143. Longest Common Subsequence/LongestCommonSubsequence.java
+++ src/1143. Longest Common Subsequence/LongestCommonSubsequence.java
@@ -1,4 +1,48 @@
+import java.util.Arrays;
+
 public class LongestCommonSubsequence {
+
+    // pending to try bottom up approach
+
+    /*
+     * Second try, again exploring every possible path but this time i use memoization to save the results of the explored paths, 
+     * so if i encounter the same path again, i can just return the saved result instead of exploring it again. 
+     * Top down approach 
+     * 
+     * Runtime: 25ms beats 29.93%
+     */    
+    class Solution2 {
+        int n;
+        int m;
+
+        public int longestCommonSubsequence(String text1, String text2) {
+            this.n = text1.length();
+            this.m = text2.length();
+            int[][] memo = new int[n][m];
+            
+            for(int i = 0; i<memo.length;i++)
+                Arrays.fill(memo[i],-1);
+
+            return longest(text1,text2,0,0,memo);
+
+        }
+
+        private int longest(String text1, String text2, int oneIdx, int twoIdx, int[][] memo){
+            if(oneIdx >= n || twoIdx >= m) return 0;
+            if(memo[oneIdx][twoIdx] != -1) return memo[oneIdx][twoIdx];
+
+            int res = (text1.charAt(oneIdx) == text2.charAt(twoIdx))
+                ? 1 + longest(text1, text2, oneIdx+1, twoIdx+1, memo)
+                : Math.max(
+                    longest(text1, text2, oneIdx+1, twoIdx, memo),
+                    longest(text1, text2, oneIdx, twoIdx+1, memo)
+                ); 
+
+            memo[oneIdx][twoIdx] = res;
+            return res;
+
+        }
+    }
 
     /*
      * First try, exploring every possible path, if the characters are equal, i increase the count and move both pointers, 
